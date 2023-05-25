66ba721c9c493f54566b44fc7a1175ad4a00681e
Boyan Botev <boyanbotev@gmail.com>
2023-05-25 20:35:39 +0100

    iPad portrait, smaller question

--- src/components/grammar-content/GrammarContent.tsx
+++ src/components/grammar-content/GrammarContent.tsx
@@ -6,9 +6,11 @@ import Button from './button/Button';
 function GrammarContent() {
     return (
         <div id="grammar">
-            <Question>
-                Yesterday, Louise ___ to the park.
-            </Question>
+            <Answers>
+                <Question>
+                    Yesterday, Louise ___ to the park.
+                </Question>
+            </Answers>
             <Answers>
                 <Button>is</Button>
                 <Button>go</Button>
